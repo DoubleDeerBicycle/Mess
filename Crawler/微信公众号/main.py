425b1135dba418f07ac856a761e0fc0bda5a790c
zhh-python <z19960814@outlook.com>
2018-10-12 10:43:09 +0800

    first commit

---
+++ Crawler/微信公众号/main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+browser = webdriver.Edge()
+browser.get('baidu.com')
