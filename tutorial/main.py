f53c36a4210a445d81e6c77cefffc01f2714efa7
zhh-python <z19960814@outlook.com>
2018-12-09 09:28:12 +0800

    crawl

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,5 +1,5 @@
 import sys,os
 from scrapy.cmdline import execute
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(['scrapy', 'crawl', 'tianya'])
+execute(['scrapy', 'crawl', 'imagebaidu'])
 # print(os.path.dirname(os.path.abspath(__file__)))
--- tutorial/tutorial/spiders/imagebaidu.py
+++ tutorial/tutorial/spiders/imagebaidu.py
@@ -16,52 +16,52 @@ class ImagebaiduSpider(scrapy.Spider):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3371.0 Safari/537.36'
     }
     # 图片关键词
-    image = '瓷砖贴图'
+    image = '门匾素材'
     url = 'https://image.baidu.com/search/acjson?'
     # 页数（每页默认30张图片）
-    page = 2 
+    page = 5 
     def parse(self, response):
         for num in range(self.page*3):
             if num % 3 == 0:
                 num = num*10
                 data = {
-                'tn': 'resultjson_com',
-                'ipn': 'rj',
-                'ct': '201326592',
-                'is': '',
-                'fp': 'result',
-                'queryWord': self.image,
-                'cl': '2',
-                'lm': '-1',
-                'ie': 'utf-8',
-                'oe': 'utf-8',
-                'adpicid': '',
-                'st': '-1',
-                'z': '',
-                'ic': '0',
-                'hd': '' ,
-                'latest': '',
-                'copyright': '',
-                'word': self.image,
-                's': '',
-                'se': '',
-                'tab': '',
-                'width': '',
-                'height': '',
-                'face': '0',
-                'istype': '2',
-                'qc': '',
-                'nc': '1',
-                'fr': '',
-                'expermode': '',
-                'selected_tags': '',
-                'pn': num,
-                'rn': '30',
-                'gsm': '1e',
-                '1543718075201': ''
-            }
-            newurl = self.url+urlencode(data)
-            yield scrapy.Request(newurl, headers=self.headers, callback=self.parse_image)
+                    'tn': 'resultjson_com',
+                    'ipn': 'rj',
+                    'ct': '201326592',
+                    'is': '',
+                    'fp': 'result',
+                    'queryWord': self.image,
+                    'cl': '2',
+                    'lm': '-1',
+                    'ie': 'utf-8',
+                    'oe': 'utf-8',
+                    'adpicid': '',
+                    'st': '-1',
+                    'z': '',
+                    'ic': '0',
+                    'hd': '' ,
+                    'latest': '',
+                    'copyright': '',
+                    'word': self.image,
+                    's': '',
+                    'se': '',
+                    'tab': '',
+                    'width': '',
+                    'height': '',
+                    'face': '0',
+                    'istype': '2',
+                    'qc': '',
+                    'nc': '1',
+                    'fr': '',
+                    'expermode': '',
+                    'selected_tags': '',
+                    'pn': num,
+                    'rn': '30',
+                    'gsm': '1e',
+                    '1543718075201': ''
+                }
+                newurl = self.url+urlencode(data)
+                yield scrapy.Request(newurl, headers=self.headers, callback=self.parse_image)
 
     def parse_image(self, response):
         image_item = ItemLoader(item=ImagebaiduItems(), response=response)
